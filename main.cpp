15bbf38a616979d559330db9e798b567fc9a6a6d
Ionel14 <53469111+Ionel14@users.noreply.github.com>
2024-01-09 23:39:32 +0200

    Homework 6 UniquePtr class (#28)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include "Sensors/lightsensor.h"
 #include "Devices/acunit.h"
 
+#include "Sensors/lightsensor.h"
+#include "Devices/acunit.h"
 #include "smarthomestatus.h"
+
+#include "uniqueptr.h"
 
 int main()
 {
@@ -32,16 +36,48 @@ int main()
 
         // smartHome.AddToXml("../data.xml");
 
-        smart_home::LightSensor light("Light", 600);
-        smart_home::AcUnit ac("AC");
+        smart_home::UniquePtr<int> ptr1(new int(1));
+        smart_home::UniquePtr<int> ptr2(new int(3));
+        
+        smart_home::UniquePtr<int> arr(new int[3]{1,2,3});
 
-        std::vector<smart_home::StatusObject*> obj;
+        smart_home::UniquePtr<int> ptr3 = nullptr;
 
-        obj.push_back(&light);
-        obj.push_back(&ac);
+        std::cout<< *ptr1<<' '<< *ptr2<<std::endl;
 
-        smart_home::SmartHomeStatus::PrintSensorsStatus(obj);
+        ptr1.swap(ptr2);
 
+        std::cout<< *ptr1<<' '<< *ptr2 << arr[1]<< std::endl;
+
+        smart_home::UniquePtr<smart_home::LightSensor> light
+        (new smart_home::LightSensor("Light", 600));
+
+        if (light)
+        {
+            std::cout<< std::endl<<light->GetIntensity()<< std::endl;
+        }
+        
+        light.reset();
+        
+        if (light)
+        {
+            std::cout<< std::endl<<light->GetIntensity()<< std::endl;
+        }
+
+        light.release();
+        
+        if (light)
+        {
+            std::cout<< std::endl<<light->GetIntensity()<< std::endl;
+        }
+        
+        smart_home::UniquePtr<smart_home::AcUnit> ac(new smart_home::AcUnit("ac"));
+        // std::vector<smart_home::StatusObject*> obj;
+
+        // obj.push_back(&light);
+        // obj.push_back(&ac);
+
+        // smart_home::SmartHomeStatus::PrintSensorsStatus(obj);
 
     }
     catch(const std::invalid_argument& e)
---
+++ uniqueptr.h
@@ -0,0 +1,112 @@
+#ifndef UNIQUEPTR_H
+#define UNIQUEPTR_H
+
+#include <memory>
+
+namespace smart_home
+{
+
+template <class T, class Deleter = std::default_delete<T>> class UniquePtr
+{
+public:
+    UniquePtr(T* ptr = nullptr, Deleter&& deleter = Deleter()) noexcept : ptr_{ptr}, deleter_{deleter}{}
+    UniquePtr(UniquePtr &&ptr): ptr_{std::move(ptr)}{}
+    UniquePtr(UniquePtr &ptr) = delete;
+    UniquePtr& operator=(const UniquePtr& ptr) = delete;
+    UniquePtr& operator=(const UniquePtr&& ptr) noexcept
+    {
+        if (this != ptr)
+        {
+            ptr_.reset(ptr.release());
+            this.deleter_ = std::move(ptr->deleter_);
+        }
+
+        return *this;
+    }
+
+    //Modifiers
+    T* release() noexcept
+    {
+        T* released = ptr_;
+        ptr_ = nullptr;
+        return released;
+    }
+
+    void reset( T* ptr = new T()) noexcept
+    {
+        T* oldPtr = this->ptr_;
+        this->ptr_= ptr;
+
+        if (oldPtr)
+        {
+            deleter_(oldPtr);
+        }    
+    }
+
+    void swap( UniquePtr& other ) noexcept
+    {
+        if(ptr_ != other.ptr_)
+        {
+            T* auxPtr = this->release();
+            ptr_ = other.release();
+            other.ptr_ = auxPtr;
+
+            auxPtr = nullptr;
+        }
+
+        Deleter& auxDel = deleter_;
+        deleter_ = other.deleter_;
+        other.deleter_ = auxDel;
+    }
+
+    //Observers
+    T* get() const noexcept
+    {
+        return ptr_;
+    }
+    Deleter& get_deleter() noexcept
+    {
+        return deleter_;
+    }
+    const Deleter& get_deleter() const noexcept
+    {
+        return deleter_;
+    }
+    
+    explicit operator bool() const noexcept
+    {
+        if (ptr_ != nullptr)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    T& operator*() const noexcept
+    {
+        return *ptr_;
+    }
+
+    T* operator->() const noexcept
+    {
+        return ptr_;
+    }
+
+    T& operator[]( std::size_t i )
+    {
+        return ptr_[i];
+    }
+
+    ~UniquePtr()
+    {
+        this->deleter_(this->ptr_);
+    }
+
+private:
+    T* ptr_;
+    Deleter deleter_;
+};
+
+} // namespace smart_home
+#endif
